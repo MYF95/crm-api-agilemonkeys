583c18c7dedd7822e3ed4cd13fa2e3840c7d0825
Mikota <myf1995@gmail.com>
2019-05-14 15:16:40 +0100

    added customer seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: 'admin@test.com', password: 'password', admin: true)
+
+customer1 = Customer.create(name: 'Ponsan', surname: 'The dog', created_by: 1, updated_by: 1)
+customer2 = Customer.create(name: 'Tsukki', surname: 'The dog', created_by: 1, updated_by: 1)
+customer1.avatar.attach({io: File.open("#{Rails.root}/public/images/default_avatar.png"), filename: 'default_avatar'})
+customer2.avatar.attach({io: File.open("#{Rails.root}/public/images/default_avatar.png"), filename: 'default_avatar'})
